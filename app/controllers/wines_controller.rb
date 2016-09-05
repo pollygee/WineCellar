6b49e37f80bcfe61aed02ea868e4cdbe617c7eb5
“Polly Sileo“ <pollysileo@gmail.com>
2016-09-04 20:48:02 -0400

    Filter wines by characteristics

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,7 +1,11 @@
 class WinesController < ApplicationController
   def index
-    @wines = Wine.all
+    # @wines = Wine.all
 
-    render json: @wines
+    filter_params = params.permit :name, :vintage, :country, :region, :color
+
+    wines = Wine.where(filter_params.to_h)
+
+    render json: wines
   end
 end
