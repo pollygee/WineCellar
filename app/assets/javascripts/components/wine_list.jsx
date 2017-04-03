6bc2066231280f232901d319bd511eb0d3ed78e5
Polly Sileo <pollysileo@gmail.com>
2017-04-02 23:38:55 -0400

    Add other attributes of wine to the table

--- app/assets/javascripts/components/wine_list.jsx
+++ app/assets/javascripts/components/wine_list.jsx
@@ -14,7 +14,10 @@ class WineList extends React.Component{
       <thead>
         <tr>
           <th>Name</th>
+          <th>Varietal</th>
           <th>Year</th>
+          <th>Rank</th>
+          <th>Cost</th>
         </tr>
       </thead>
       <tbody>
@@ -24,7 +27,10 @@ class WineList extends React.Component{
         )).map(function(wine){
           return <tr key={wine.id}>
           <td>{wine.name}</td>
+          <td>{wine.varietal}</td>
           <td>{wine.vintage}</td>
+          <td>{wine.rank}</td>
+          <td>{wine.cost}</td>
           </tr>
         })
       }
