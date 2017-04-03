65fff4820b5604066b69c61cef58212b7cce009b
Polly Sileo <pollysileo@gmail.com>
2017-04-02 23:15:37 -0400

    Ignore wines with no name

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,7 @@
 class Wine < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
+      next unless row["name"]
       Wine.create! row.to_hash
     end
   end
